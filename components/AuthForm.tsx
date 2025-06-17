7d29425d48825a8587cb625c1bbf4fd4b1eb01a1
Aditya <52035504+aditya-vinodh@users.noreply.github.com>
2025-06-17 18:49:34 +0530

    Updated AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -63,6 +63,38 @@ export default function AuthForm({ mode, onToggleMode }: AuthFormProps) {
         )}
         
         <form onSubmit={handleSubmit} className="space-y-4">
+          {mode == 'login' ? () : (
+            <div className="grid grid-cols-2">
+              <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-gray-900 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-500"
+              placeholder="your@email.com"
+            />
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm text-gray-900 bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-500"
+              placeholder="your@email.com"
+            />
+              </div>
+            </div>
+          )}
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">
               Email
